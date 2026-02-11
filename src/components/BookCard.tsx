3b4c8258cd3a07dc2afe2d4c36f9e6312a36a943
AK <adikraid1234@gmail.com>
2026-02-11 21:45:42 +0200

    feat: implement BookCard component with like, edit, and delete functionalities

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,7 +1,58 @@
-export function BookCard() {
+import { Card, Button } from "flowbite-react";
+import { FaHeart, FaTrashAlt } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
+import { BookResponse } from "../models/BookCard.types";
+import { useState, useEffect } from "react";
+interface BookCardProps {
+  book: BookResponse;
+  onLike: (book: BookResponse) => void;
+  onEdit: (book: BookResponse) => void;
+  onDelete: (id: string) => void;
+}
+
+function BookCard({ book, onLike, onDelete, onEdit }: BookCardProps) {
+  const [isFavorite, setIsFavorite] = useState(book.isFavorite);
+
+  useEffect(() => {
+    setIsFavorite(book.isFavorite);
+  }, [book.isFavorite]);
+
+  const handleLike = () => {
+    setIsFavorite(!isFavorite);
+    onLike({ ...book, isFavorite: !isFavorite });
+  };
+  const handleEdit = () => {
+    onEdit(book);
+  };
+  const handleDelete = () => {
+    onDelete(book.id);
+  };
+
   return (
-    <>
-      <span>ice cream</span>
-    </>
+    <Card className="flex h-full flex-col text-center">
+      <div className="rounded-t-lg bg-gray-900">
+        <img
+          className="h-60 w-full object-contain"
+          src={book.coverImage}
+          alt={book.title}
+        />
+      </div>
+      <h1 className="truncate text-2xl font-bold">{book.title}</h1>
+      <p className="truncate text-gray-500">{book.author}</p>
+      <p className="mt-2 line-clamp-5 grow">{book.description}</p>
+      <span className="flex flex-row justify-between">
+        <Button color={isFavorite ? "green" : "gray"} onClick={handleLike}>
+          <FaHeart />
+        </Button>
+        <Button color={"blue"} onClick={handleEdit}>
+          <MdEdit />
+        </Button>
+        <Button color={"red"} onClick={handleDelete}>
+          <FaTrashAlt />
+        </Button>
+      </span>
+    </Card>
   );
 }
+
+export default BookCard;
